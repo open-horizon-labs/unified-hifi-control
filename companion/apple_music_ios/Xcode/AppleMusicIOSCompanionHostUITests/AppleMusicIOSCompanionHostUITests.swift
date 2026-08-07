dd0ab17439e71d0e8177cd2d404401aa87688e63
Muness Castle <munesscastle@artium.ai>
2026-08-07 10:21:39 -0400

    test: remove placeholder iOS UI test

--- companion/apple_music_ios/Xcode/AppleMusicIOSCompanionHostUITests/AppleMusicIOSCompanionHostUITests.swift
+++ companion/apple_music_ios/Xcode/AppleMusicIOSCompanionHostUITests/AppleMusicIOSCompanionHostUITests.swift
@@ -15,16 +15,6 @@ final class AppleMusicIOSCompanionHostUITests: XCTestCase {
     }
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertTrue(true)
-    }
-
-    @MainActor
     func testAirPlayRoutePickerIsAvailable() throws {
         let app = XCUIApplication()
         app.launch()
